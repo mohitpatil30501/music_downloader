46caf8b49f1e3de14069ac518ce7ee34920a6232
mohitpatil30501 <mohitpatil30501@gmail.com>
2020-10-13 01:29:35 +0530

    Edited project from zip download to indiviual file download & song download successfully

--- modules/hindi.py
+++ modules/hindi.py
@@ -8,12 +8,14 @@ class Hindi:
         self.month = int(input("Enter Month: "))
         self.year = int(input("Enter Year: "))
         self.album_list = []
-        self.path_to_zip = self.path_to_extract = None
 
     def fetch_album_list(self):
         try:
             request = requests.get("https://jatt.net")
             baseurl = request.url
+            if request is None:
+                print("No Data found....Try again...!")
+                exit(0)
         except:
             print("Error to Connect Site....Try again...!")
             exit(0)
@@ -21,22 +23,110 @@ class Hindi:
         try:
             request_url = str(baseurl) + "/month.php?c=Hindi&m=" + str(self.month) + "&y=" + str(self.year)
             request = requests.get(request_url)
+            if request is None:
+                print("No Data found....Try again...!")
+                exit(0)
         except:
-            print("Error to Get Data...Try again...!")
+            print("Error to Extract Data...Try again...!")
             exit(0)
 
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
         for single_element in soup.find_all("a", {"class": "touch"}):
             removes = single_element.find("font", {"color": "#339900"})
+
+            list_song = Hindi.find_song(baseurl + single_element["href"])
+            if list_song is None:
+                print("Not Downloadable file Founded...!")
+                continue
+
             self.album_list.append({
-                "Name": (single_element.text.strip().replace("»", "").replace(removes.text.strip(), "")).strip(),
-                "Link": baseurl+single_element["href"]
+                "Album_name": (single_element.text.strip().replace("»", "").replace(removes.text.strip(), "")).strip(),
+                "Link": baseurl + single_element["href"],
+                "Songs": list_song
             })
 
-    def download_zip(self):
-        pass
+        self.download()
 
-    def unzip_zipfile(self):
-        with zipfile.ZipFile(self.path_to_zip, 'r') as zip_element:
-            zip_element.extractall(self.path_to_extract)
+    @staticmethod
+    def find_song(link):
+        try:
+            request = requests.get(link)
+            if request is None:
+                print("No Data found....Try again...!")
+                exit(0)
+        except:
+            print("Error to Connect Site....Try again...!")
+            exit(0)
+
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        list_song = []
+        for song in soup.find_all("a", {"class": "touch"}):
+            if song.text.strip() != "Play All":
+                data = {
+                    "Song_name": song.text.strip(),
+                    "Link": Hindi.find_song_link(song["href"])
+                }
+                list_song.append(data)
+        return list_song
+
+    @staticmethod
+    def find_song_link(link):
+        try:
+            request = requests.get(link)
+            if request is None:
+                print("No Data found....Try again...!")
+                exit(0)
+        except:
+            print("Error to Connect Site....Try again...!")
+            exit(0)
+
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        song_link = None
+        for song in soup.find_all("a", {"class": "touch"}):
+            removes = song.find("font", {"color": "#339900;"})
+
+            if removes is not None:
+                data = song.text.strip().replace(removes.text.strip(), "").strip()
+
+                if data == "Download in 320 kbps":
+                    song_link = song["href"]
+                elif data == "Download in 128 kbps":
+                    song_link = song["href"]
+                elif data == "Download in 48 kbps":
+                    song_link = song["href"]
+        return song_link
+
+    def download(self):
+        print("Downloading...")
+        if not os.path.isdir('Songs'):
+            os.mkdir('Songs')
+        os.chdir('Songs')
+
+        for album in self.album_list:
+            os.mkdir(album["Album_name"])
+            print(album["Album_name"], " Album stared downloading...")
+            song_list = album['Songs']
+            for song in song_list:
+                status = True
+                print(song["Song_name"], "stared downloading...")
+                n = 0
+                while status and n <= 3:
+                    try:
+                        song_link = song['Link']
+                        request = requests.get(song_link)
+                        break
+                    except:
+                        if n < 3:
+                            n += 1
+                            print("Trying Again...(", n, ")")
+                        else:
+                            print('Error to download File of "', album['Album_name'], '"\nTry Again..!')
+                            status = False
+                if status:
+                    filename = os.path.join(os.getcwd(), album["Album_name"], song["Song_name"])
+                    with open(filename + ".mp3", "wb") as file:
+                        file.write(request.content)
+                    print("Download completed...")
